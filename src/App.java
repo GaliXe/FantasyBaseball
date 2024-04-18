40d4b5c25dac61ee941e4a6aa8a716cb778b6be2
nkaspar3 <nicholas.j.kasparek@gmail.com>
2024-04-18 13:36:00 -0400

    ui created

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 import Features.Database;
 import Features.DraftManager;
@@ -8,8 +9,20 @@ public class App {
     static DraftManager dm = new DraftManager();
 
     public static void main(String[] args) throws IOException {
+        Scanner keyboard = new Scanner(System.in);
         db.initalizeDatabase();
         dm.intializeDraftTeams();
+
+        String input = keyboard.nextLine();
+        String[] values = input.split(" ");
+
+        switch(values[0].toLowerCase()){
+            case "list":
+                dm.listPlayers();
+                break;
+            default: System.out.println("No command found. If you need help, type in HELP for a list of commands");
+                break;
+        }
         
         dm.listPlayers();
     }
